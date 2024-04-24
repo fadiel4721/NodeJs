b8062fd61477e39049f482ca449ccfe128432213
fadiel4721 <fadielintip@gmail.com>
2024-04-24 16:21:45 +0700

    first commit

---
+++ crud-mahasiswa/routes/mahasiswa.js
@@ -0,0 +1,14 @@
+// membuat variable router dengan require atau export variabel express
+// Dan menggunakan metode Router
+const router = require("express").Router();
+// export controller yang ingin dipakai
+const mahasiswaController = require("../controllers/mahasiswaController");
+
+// endpoint mahasiswa
+router.get("/", mahasiswaController.viewMahasiswa); // Untuk view
+router.post("/", mahasiswaController.addMahasiswa); // Untuk menambahkan data mahasiswa
+router.put("/", mahasiswaController.editMahasiswa); // Untuk edit data mahasiswa
+router.delete("/:id", mahasiswaController.deleteMahasiswa); // Untuk hapus data mahasiswa
+
+// Lalu export routernya
+module.exports = router;
